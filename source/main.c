d743b07467075a6b591944b846ff0fe70eae8dcf
MRyali <mayurryali29@gmail.com>
2020-05-16 17:17:33 -0700

    Chaged logic in part 1

--- source/main.c
+++ source/main.c
@@ -21,36 +21,36 @@ void set_PWM(double frequency) {
 	// Will only update the registers when the frequency changes, otherwise allows
 	// music to play uninterrupted.
 	if (frequency != current_frequency) {
-		if (!frequency) { TCCR0B &= 0x08; } //stops timer/counter
-		else { TCCR0B |= 0x03; } // resumes/continues timer/counter
+		if (!frequency) { TCCR3B &= 0x08; } //stops timer/counter
+		else { TCCR3B |= 0x03; } // resumes/continues timer/counter
 
 		// prevents OCR3A from overflowing, using prescaler 64
 		// 0.954 is smallest frequency that will not result in overflow
-		if (frequency < 0.954) { OCR0A = 0xFFFF; }
+		if (frequency < 0.954) { OCR3A = 0xFFFF; }
 
 		// prevents OCR0A from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
-		else if (frequency > 31250) { OCR0A = 0x0000; }
+		else if (frequency > 31250) { OCR3A = 0x0000; }
 
 		// set OCR3A based on desired frequency
-		else { OCR0A = (short)(8000000 / (128 * frequency)) - 1; }
+		else { OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
 
-		TCNT0 = 0; // resets counter
+		TCNT3 = 0; // resets counter
 		current_frequency = frequency; // Updates the current frequency
 	}
 }
 
 void PWM_on() {
-	TCCR0A = (1 << COM0A0 | 1 << WGM00);
+	TCCR3A = (1 << COM3A0);
 	// COM3A0: Toggle PB3 on compare match between counter and OCR0A
-	TCCR0B = (1 << WGM02) | (1 << CS01) | (1 << CS00);
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
 	// WGM02: When counter (TCNT0) matches OCR0A, reset counter
 	// CS01 & CS30: Set a prescaler of 64
 	set_PWM(0);
 }
 
 void PWM_off() {
-	TCCR0A = 0x00;
-	TCCR0B = 0x00;
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
 }
 
 enum States {Start, wait, c4, d4, e4} state;
@@ -127,18 +127,18 @@ void Tick() {
 }
 
 int main(void) {
-    DDRA = 0x00; PORTA = 0xFF; // input
-	DDRB = 0xFF; PORTB = 0x00; // output
-
-    PWM_on();
-	state = Start;
-
-    while (1) {
-        button0 = ~PINA & 0x01;
-        button1 = ~PINA & 0x02;
-        button2 = ~PINA & 0x04;
-
-        Tick();
-    }
-    return 1;
-}
+	DDRA = 0x00; PORTA = 0xFF; // input
+/*	Author: Mayur Ryali
+ *  Partner(s) Name:
+ *	Lab Section: 21
+ *	Assignment: Lab #9  Exercise #3
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
